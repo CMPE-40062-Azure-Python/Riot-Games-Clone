12d2a2d192d6ba2bbdb5be48cabc063d8b275c72
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2023-10-20 19:24:40 +0800

    Input all html content for who-are-we webpage

--- my_new_flask_app/app.py
+++ my_new_flask_app/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/who-are-we')
+def who_are_we():
+    return render_template('who-are-we.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
