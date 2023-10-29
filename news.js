e8338c2030ee1f1870413d5d3e3880c62b0de071
BryceMadelo <brycemadelo07@gmail.com>
2023-10-29 17:41:57 +0800

    Filter - Everything and Offices Toggle

--- news.js
+++ news.js
@@ -55,4 +55,61 @@ scrollToTop.addEventListener('click', () => {
   });
 
 
+  //News Filter
+
+  const Everything = document.getElementById("everything")
+  const Offices = document.getElementById("offices");
+  const Disciplines = document.getElementById("discipline");
+  const News = document.getElementById("news");
+  const Inside_riot = document.getElementById("inside_riot");
+  const One_Shot = document.getElementById("one-shot");
+  const Tech_Blog = document.getElementById("tech_blog");
+  
+
+  const CustomResponsive = document.getElementById("custom-box-Responsive")
+
+  const customBox1 = document.getElementById("custom-box-1");
+  const customBox2 = document.getElementById("custom-box-2");
+  const customBox3 = document.getElementById("custom-box-3");
+  const customBox4 = document.getElementById("custom-box-4");
+  const customBox5 = document.getElementById("custom-box-5");
+  const customBox6 = document.getElementById("custom-box-6");
+  const customBox7 = document.getElementById("custom-box-7");
+  const customBox8 = document.getElementById("custom-box-8");
+
+  Everything.addEventListener('click', function () {
+    CustomResponsive.style.top = '220px';
+    customBox1.style.top = '1290px';
+    customBox2.style.top = '1290px';
+    customBox3.style.top = '2000px';
+    customBox4.style.top = '2000px';
+    document.getElementById('everythingDefault').textContent = 'Everything';
+    options.style.display = 'none';
+  })
+  
+Offices.addEventListener('click', function () {
+    CustomResponsive.style.top = '880px';
+    customBox1.style.top = '2000px';
+    customBox2.style.top = '2000px';
+    customBox3.style.top = '220px';
+    customBox4.style.top = '220px';
+    document.getElementById('everythingDefault').textContent = 'Offices';
+    options.style.display = 'none';
+});
+
+/*Offices.addEventListener('click', () => {
+    customBox3.style.display = 'block';
+    customBox4.style.display = 'block';
+    customBox1.style.display = 'none';
+    customBox2.style.display = 'none';
+    customBox5.style.display = 'none';
+    customBox6.style.display = 'none';
+    customBox7.style.display = 'none';
+    customBox8.style.display = 'none';
+    options.style.display = 'none'
+});*/
+
+
+
+
   
