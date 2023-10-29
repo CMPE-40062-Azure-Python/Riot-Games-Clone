5d026af6390b67c48cfb10cfa14c77c1853b4dda
BryceMadelo <brycemadelo07@gmail.com>
2023-10-29 19:33:37 +0800

    the rest of the filter

--- news.js
+++ news.js
@@ -59,10 +59,10 @@ scrollToTop.addEventListener('click', () => {
 
   const Everything = document.getElementById("everything")
   const Offices = document.getElementById("offices");
-  const Disciplines = document.getElementById("discipline");
+  const Disciplines = document.getElementById("disciplines");
   const News = document.getElementById("news");
   const Inside_riot = document.getElementById("inside_riot");
-  const One_Shot = document.getElementById("one-shot");
+  const One_Shot = document.getElementById("one_shot");
   const Tech_Blog = document.getElementById("tech_blog");
   
 
@@ -83,33 +83,93 @@ scrollToTop.addEventListener('click', () => {
     customBox2.style.top = '1290px';
     customBox3.style.top = '2000px';
     customBox4.style.top = '2000px';
+    customBox5.style.top = '2700px';
+    customBox6.style.top = '2700px';
+    customBox7.style.top = '3500px';
+    customBox8.style.top = '3500px';
     document.getElementById('everythingDefault').textContent = 'Everything';
     options.style.display = 'none';
   })
   
 Offices.addEventListener('click', function () {
-    CustomResponsive.style.top = '880px';
-    customBox1.style.top = '2000px';
-    customBox2.style.top = '2000px';
+    CustomResponsive.style.top = '1000px';
+    customBox1.style.top = '2110px';
+    customBox2.style.top = '2110px';
     customBox3.style.top = '220px';
     customBox4.style.top = '220px';
+    customBox5.style.top = '2810px';
+    customBox6.style.top = '2810px';
+    customBox7.style.top = '3550px';
+    customBox8.style.top = '3550px';
     document.getElementById('everythingDefault').textContent = 'Offices';
     options.style.display = 'none';
 });
 
-/*Offices.addEventListener('click', () => {
-    customBox3.style.display = 'block';
-    customBox4.style.display = 'block';
-    customBox1.style.display = 'none';
-    customBox2.style.display = 'none';
-    customBox5.style.display = 'none';
-    customBox6.style.display = 'none';
-    customBox7.style.display = 'none';
-    customBox8.style.display = 'none';
-    options.style.display = 'none'
-});*/
+Disciplines.addEventListener('click', function () {
+    CustomResponsive.style.top = '1000px';
+    customBox1.style.top = '2050px';
+    customBox2.style.top = '2050px';
+    customBox3.style.top = '2760px';
+    customBox4.style.top = '2760px';
+    customBox5.style.top = '3400px';
+    customBox6.style.top = '3400px';
+    customBox7.style.top = '220px';
+    customBox8.style.top = '220px';
+    document.getElementById('everythingDefault').textContent = 'Disciplines';
+    options.style.display = 'none';
+});  
 
+News.addEventListener('click', function () {
+    CustomResponsive.style.top = '1000px';
+    customBox1.style.top = '2050px';
+    customBox2.style.top = '2050px';
+    customBox3.style.top = '2760px';
+    customBox4.style.top = '2760px';
+    customBox5.style.top = '220px';
+    customBox6.style.top = '220px';
+    customBox7.style.top = '3500px';
+    customBox8.style.top = '3500px';
+    document.getElementById('everythingDefault').textContent = 'News';
+    options.style.display = 'none';
+});
 
+Inside_riot.addEventListener('click', function () {
+    CustomResponsive.style.top = '900px';
+    customBox1.style.top = '220px';
+    customBox2.style.top = '220px';
+    customBox3.style.top = '2050px';
+    customBox4.style.top = '2050px';
+    customBox5.style.top = '2760px';
+    customBox6.style.top = '2760px';
+    customBox7.style.top = '3500px';
+    customBox8.style.top = '3500px';
+    document.getElementById('everythingDefault').textContent = 'Inside Riot';
+    options.style.display = 'none';
+})
+One_Shot.addEventListener('click', function () {
+    CustomResponsive.style.top = '1000px';
+    customBox1.style.top = '2110px';
+    customBox2.style.top = '2110px';
+    customBox3.style.top = '220px';
+    customBox4.style.top = '220px';
+    customBox5.style.top = '2810px';
+    customBox6.style.top = '2810px';
+    customBox7.style.top = '3550px';
+    customBox8.style.top = '3550px';
+    document.getElementById('everythingDefault').textContent = 'One Shot';
+    options.style.display = 'none';
+})
 
-
-  
+Tech_Blog.addEventListener('click', function () {
+    CustomResponsive.style.top = '900px';
+    customBox1.style.top = '220px';
+    customBox2.style.top = '220px';
+    customBox3.style.top = '2050px';
+    customBox4.style.top = '2050px';
+    customBox5.style.top = '2760px';
+    customBox6.style.top = '2760px';
+    customBox7.style.top = '3500px';
+    customBox8.style.top = '3500px';
+    document.getElementById('everythingDefault').textContent = 'Tech Blog';
+    options.style.display = 'none';
+})
